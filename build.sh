6ae0f28e60421c1c6ab7851ff4bb49784db1ba4d
LeePu <leepupu@gmail.com>
2016-06-01 16:28:19 +0800

    build with debug symbol

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 rm ./MYCPU.out
-g++ mycpu.cpp main.cpp program.cpp cpu.cpp pipeline_reg.cpp control_signal.cpp data_memory.cpp instruction.cpp register.cpp -o MYCPU.out
+g++ -g mycpu.cpp main.cpp program.cpp cpu.cpp pipeline_reg.cpp control_signal.cpp data_memory.cpp instruction.cpp register.cpp -o MYCPU.out
 ./MYCPU.out
